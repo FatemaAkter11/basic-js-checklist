b6585179429bcd5ec6aa1d50044e2bbd69787f90
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-07 22:52:28 +0600

    difference type of variables

---
+++ variables.js
@@ -0,0 +1,21 @@
+// write 3 variables (number, string, boolean)
+
+var name = 'Rahim';
+console.log(typeof name);
+
+var number = 4;
+console.log(typeof number);
+
+var isTrue = false;
+console.log(typeof isTrue);
+
+// write 2 variables (let,const)
+
+let price = 134;
+price = 24;
+price = 56;
+console.log(price);
+
+const myName = 'Belal';
+// myName = 'akbar'; // error
+console.log(myName);
