a37149ba5d10ade1f426aefb3f1b9a7464eee9bb
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-07 23:00:16 +0600

    simple mathematical term

---
+++ simple-math.js
@@ -0,0 +1,29 @@
+//Addition
+var num1 = 20;
+var num2 = 30;
+var totalAdd = num1 + num2;
+console.log('Addition=', totalAdd);
+
+//subtraction
+var num1 = 40;
+var num2 = 30;
+var totalSub = num1 - num2;
+console.log('subtraction=', totalSub);
+
+//Division
+var num1 = 20;
+var num2 = 30;
+var totalDiv = num1 / num2;
+console.log('Division=', totalDiv.toFixed(1));
+
+//Multiplication
+var num1 = 20;
+var num2 = 30;
+var totalMult = num1 * num2;
+console.log('Multiplication=', totalMult);
+
+//Remainder
+var num1 = 17;
+var num2 = 3;
+var totalRemainder = num1 % num2;
+console.log('Remainder=', totalRemainder);
