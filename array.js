203066b71d9729f5dcd350e194cebce31fde723a
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-08 00:10:32 +0600

    array

---
+++ array.js
@@ -0,0 +1,11 @@
+let nameArray = ['Rahim', 'Karim', 'judu', 'labu', 'habu'];
+
+console.log(nameArray.length);
+console.log(nameArray.indexOf('Karim'));
+//nameArray.push('kudu');
+//nameArray.push('ludu');
+//nameArray.pop();
+nameArray[4] = 'gadu';
+
+console.log(nameArray);
+
