0b0bad9c8016c33280460eb99576a1f4217ef823
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-07 23:24:33 +0600

    comparison variables

---
+++ comparison-variables.js
@@ -0,0 +1,27 @@
+//Comparison 2 variables
+let num1 = 12;
+let num2 = 13
+console.log('Is it Equal=', num1 == num2);
+
+console.log('Is it not Equal=', num1 != num2);
+
+console.log('Is it greater than=', num1 > num2);
+
+console.log('Is it less than=', num1 < num2);
+
+console.log('Is it greater than or equal to=', num1 >= num2);
+
+console.log('Is it less than or equal to=', num1 <= num2);
+
+
+// Two conditions case:1 fulfil both conditions
+
+var myLove = 98;
+var yourLove = 99;
+
+console.log('Love=', myLove && yourLove);
+
+// Two conditions case:1 fulfil at least one conditions
+let myBook = 'titanic';
+let yourBook = 'algebra';
+console.log('Love=', myBook || yourBook);
