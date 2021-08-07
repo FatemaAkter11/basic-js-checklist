a9f1ab21f07bf065ed0221df2181015bd71137af
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-07 23:34:19 +0600

    if else condition practice

---
+++ if-else.js
@@ -0,0 +1,22 @@
+
+//if---else
+
+var num1 = 300;
+if (num1 < 40) {
+    console.log('num1 is less than 40');
+}
+else {
+    console.log('num1 is greater than 40');
+}
+
+
+//if---else---else if
+
+let time = 28;
+if (time < 10) {
+    console.log("Good morning");
+} else if (time < 20) {
+    console.log("Good day");
+} else {
+    console.log("Good evening");
+}
