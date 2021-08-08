117ba63f71ae96db6419b4e3c69760c14a9eb03b
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-08 20:16:20 +0600

    2nd module problem 2

---
+++ centimeterToMeter.js
@@ -0,0 +1,10 @@
+//centimeterToMeter.js
+// 1 centimeter = 0.01 meter
+// Formula :  meter = centimeter * 0.01;
+
+function centimeterToMeter(centimeter) {
+    let meter = centimeter * 0.01;
+    return meter;
+}
+const result = centimeterToMeter(20);
+console.log(result);
