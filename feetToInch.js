cf5064332e0f0a93aaecbdd58b6eca9df5ac7230
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-08 20:09:16 +0600

    2nd module problem 1

---
+++ feetToInch.js
@@ -0,0 +1,10 @@
+// Conversation feet to inches using function
+// 1 feet = 12 inches
+//Feet to Inches formula : inch = feet * 12
+
+function feetToInch(feet) {
+    let inch = feet * 12;
+    return inch;
+}
+const result = feetToInch(2);
+console.log(result);
