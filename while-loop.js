0a9dbde2b33966fee689ee800c279cf411053c6a
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-08 19:38:13 +0600

    problem 7

--- while-loop.js
+++ while-loop.js
@@ -1,7 +1,17 @@
-//odd numbers
-let i = 7;
+//// Write a Program Using While Loop Where Loop Will be Start form 7 to 19.(odd numbers)
+/* let i = 7;
 let num = 0;
 while (i <= 19) {
     console.log(i);
     i += 2;
+} */
+
+
+//another way(odd numbers)
+let i = 7;
+while (i <= 19) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+    i++;
 }
