4faad5c60d4dfd6490c25bc7d0497051b43763a9
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-08 19:33:16 +0600

    problem 4 & 5

--- comparison-variables.js
+++ comparison-variables.js
@@ -1,4 +1,4 @@
-//Comparison 2 variables
+//Comparison 2 variables (<, >, == , != , <=, >=)
 let num1 = 12;
 let num2 = 13
 console.log('Is it Equal=', num1 == num2);
@@ -14,14 +14,15 @@ console.log('Is it greater than or equal to=', num1 >= num2);
 console.log('Is it less than or equal to=', num1 <= num2);
 
 
-// Two conditions case:1 fulfil both conditions
+// Two conditions case:1 fulfil both conditions and // Two conditions case:1 fulfil at least one conditions
 
 var myLove = 98;
-var yourLove = 99;
+var yourLove = 97;
 
-console.log('Love=', myLove && yourLove);
+if (myLove <= 100 && yourLove >= 98) {
+    console.log('we are happy couple');
+}
+else if (myLove >= 99 || yourLove <= 100) {
+    console.log('we are unhappy couple');
+}
 
-// Two conditions case:1 fulfil at least one conditions
-let myBook = 'titanic';
-let yourBook = 'algebra';
-console.log('Love=', myBook || yourBook);
