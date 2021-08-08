83ea79c7a09e13984489fcb9dde62985869cc2f2
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-08-08 19:50:11 +0600

    problem 8 & 10

--- array.js
+++ array.js
@@ -1,15 +1,28 @@
 /* Declare an Array and find the number of element, Update element in 4th Position, add and remove element, find spesific element in the array */
 
-let nameArray = ['Rahim', 'Karim', 'judu', 'labu', 'habu'];
+/* let nameArray = ['Rahim', 'Karim', 'judu', 'labu', 'habu'];
 
 console.log(nameArray.length);
 console.log(nameArray.indexOf('Karim'));
-//nameArray.push('kudu');
-//nameArray.push('ludu');
-//nameArray.pop();
+nameArray.push('kudu');
+nameArray.push('ludu');
+nameArray.pop();
 nameArray[4] = 'gadu';
 
-//console.log(nameArray);
+console.log(nameArray); */
+
+
+// You have an array of Number. Display only the number bigger then 80
+
+let arrayNumber = [17, 10, 20, 82, 90, 50, 80, 110, 15, 170, 15, 100];
+for (let i = 0; i < arrayNumber.length; i++) {
+    let elements = arrayNumber[i];
+    if (elements > 80) {
+        console.log(elements);
+    }
+}
+
+
 
 
 
